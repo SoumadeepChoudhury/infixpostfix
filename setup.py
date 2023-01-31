acbcd6f5baeb2fd055e8162270c4524dbd72849f
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2023-01-31 18:56:36 +0530

    Updated content and README

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup,find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="infix-postfix",
     version="1.0.0",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Ahens | An Initiative to Initial',
     author_email="ahensinitiative@gmail.com",
     packages=find_packages(),
